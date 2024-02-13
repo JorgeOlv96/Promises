985d6d465243f35aa067ab2c7a7b563e4a3689c9
JorgeOlv96 <2020171010@uteq.edu.mx>
2024-02-12 19:34:30 -0600

    Promesas_3

--- Promesas/Promesas.js
+++ Promesas/Promesas.js
@@ -1,11 +1,29 @@
-let promesaConError = new Promise((resolve, reject) => {
-    // Simulando un error durante la ejecución
-    reject('Algo salió mal...');
-});
+const cargarDatosDeApi = (url) => {
+    return new Promise((resolve, reject) => {
+        // Simula una solicitud a una API
+        fetch(url)
+            .then(response => {
+                console.log(`Estado de la respuesta: ${response.status}`)
+                if (response.status === 200) {  
+                    return response.json()
+                } else {
+                    throw new Error(`Error en la solicitud: ${response.status}`)
+                }
+            })
+            .then(data => resolve(data))
+            .catch(error => {
+                reject(`Error en la solicitud: ${error.message}`)
+            })
+    })
+}
 
-// Manejando la promesa con error
-promesaConError.then((mensaje) => {
-    console.log(mensaje);
-}).catch((error) => {
-    console.error(error);
-});
+// Uso de la función para cargar los datos de una API
+const apiURL = 'https://jsonplaceholder.typicode.com/users'
+
+cargarDatosDeApi(apiURL)
+    .then(data => {
+        console.log(`Datos de la API:`, data)
+    })
+    .catch(error => {
+        console.log(`Error en la solicitud: ${error}`)
+    })
