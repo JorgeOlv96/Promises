57f4916f258ccf5cdbb5a6e1cde2e0d39fa9e20d
JorgeOlv96 <2020171010@uteq.edu.mx>
2024-02-12 19:57:00 -0600

    Peticiones_Fecth6

--- Promesas/Promesas.js
+++ Promesas/Promesas.js
@@ -1,24 +1,6 @@
-const sumarLento = (numero) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(numero + 1);
-            // reject('Sumar lento fallo');
-        }, 800);
+// Solicitud a una API con Fetch
+fetch(resp => {
+    resp.json().then(data => {
+        console.log(data);
     });
-}
-
-const sumarRapido = (numero) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(numero + 1);
-        }, 300);
-    });
-}
-
-// Promise.race compite entre las promesas y devuelve la primera que se resuelve
-// Si falla una, fallan todas
-Promise.race([sumarLento(5), sumarRapido(10)])
-    .then(respuestas => {
-        console.log(respuestas);
-    })
-    .catch(console.log);
+})
